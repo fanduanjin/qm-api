59fecc9e371e454d66d45e9e26b9b4af72a8169a
fanduanjin <786793542@qq.com>
2022-03-11 21:42:12 +0800

    getDetail部分BUG修改

--- api/getSingerDetail.js
+++ api/getSingerDetail.js
@@ -1,7 +1,6 @@
 const { Singer } = require('../model/Singer')
 const md5 = require('js-md5')
 const retryRequest = require('../util/retryRequest')
-
 const parseSingerInfo = async (data) => {
     let singer = new Singer()
     //解析 basic_info
@@ -17,6 +16,32 @@ const parseSingerInfo = async (data) => {
     singer.genre = data.ex_info.genre
     singer.foreignName = data.ex_info.foreign_name
     singer.birthday = data.ex_info.birthday
+    if (singer.birthday) {
+        singer.birthday = singer.birthday.replace(/\//g, '-')
+        //判断日期格式长度是否标准 2022-02-02  10位
+        if (singer.birthday.length != 10) {
+            //不标准 设置标准
+            let ts = singer.birthday.split('-')
+            if (ts[0].length < 4) {
+                //月
+                for (i = 0, l = 4 - ts[0].length; i < l; i++) {
+                    ts[0] = ts[0] + 0
+                }
+            }else if(ts[0].length > 4){
+                ts[0]=ts[0].substr(0,4)
+                
+            }
+            if (ts[1].length != 2) {
+                //月
+                ts[1] = 0 + ts[1]
+            }
+            if (ts[2].length != 2) {
+                //日
+                ts[2] = 0 + ts[2]
+            }
+            singer.birthday = ts.join('-')
+        }
+    }
     if (data.wiki)
         singer.wiki = md5(data.wiki)
     //判断是否为 组合，如果是组合 解析组合信息
@@ -24,7 +49,7 @@ const parseSingerInfo = async (data) => {
         singer.team = []
         for (let index in data.group_list) {
             let item = data.group_list[index]
-            let teamSinger = new Singer.Singer()
+            let teamSinger = new Singer()
             teamSinger.id = item.singer_id
             teamSinger.name = item.name
             teamSinger.mid = item.singer_mid
